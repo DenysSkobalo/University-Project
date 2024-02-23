a9ba540a1f7c2e96dc4a2baa01fa94711c520165
DenysSkobalo <denysskobalodev@gmail.com>
2024-02-23 01:17:16 +0100

    Added all player movement animations

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+#@onready var anim = get_node("AnimatedSprite2D")
+@onready var anim = $AnimatedSprite2D
+
 var direction: Vector2 = Vector2.ZERO
 @export var speed: int = 150
 
@@ -8,4 +11,18 @@ func _process(delta):
 
 func _physics_process(delta):
 	velocity = direction * speed
+	if direction != Vector2.ZERO:
+		if direction.y < 0:
+			anim.play("Up Walk"	)
+		elif direction.y > 0:
+			anim.play("Down Walk")
+		elif direction.x != 0:
+			anim.play("Side Walk")
+			anim.flip_h = direction.x > 0
+	else: 
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+		anim.play("Down Idle")
+		anim.flip_h = false
+		
 	move_and_slide()
