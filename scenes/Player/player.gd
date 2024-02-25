356f0109ff7f609867cf40ae8420cc5437516df8
DenysSkobalo <denysskobalodev@gmail.com>
2024-02-25 12:11:16 +0100

    added a campfire and its animation

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,22 +1,31 @@
 extends CharacterBody2D
 
+# Accessing the AnimatedSprite2D node
 #@onready var anim = get_node("AnimatedSprite2D")
 @onready var anim = $AnimatedSprite2D
+
+# Speed variable for the character
 @export var speed: int = 150
 
+# Variable to store the current direction of character movement
 var direction: Vector2 = Vector2.ZERO
-# Save last direction move
+# Variable to store the last direction of character movement
 var last_direction: Vector2 = Vector2.DOWN
 
+# Processing user input
 func _process(delta):
 	direction = Input.get_vector("Left","Right","Up","Down")
 
+# Physics processing for movement and animation
 func _physics_process(delta):
+	# Calculating velocity based on direction
 	velocity = direction * speed
 	
+	# Checking if the character is moving
 	if direction != Vector2.ZERO:
 		last_direction = direction
 		
+		# Choosing animation based on movement direction
 		if direction.y < 0:
 			anim.play("Up Walk"	)
 		elif direction.y > 0:
@@ -25,9 +34,11 @@ func _physics_process(delta):
 			anim.play("Side Walk")
 			anim.flip_h = direction.x > 0
 	else: 
+		# Gradually reducing speed to zero
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.y = move_toward(velocity.y, 0, speed)
 		
+		# Choosing the Idle animation based on the last direction of movement
 		if last_direction.y < 0: 
 			anim.play("Up Idle")
 		elif last_direction.y > 0:
